7b31bd09870a794dc8b5b492fdccb666d0988fd5
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-01-27 16:41:01 +0100

    Arrows added

--- shapes.lua
+++ shapes.lua
@@ -1,8 +1,12 @@
 shapes = {}
-shapes.newTriangle = {}
+shapes.triangle = {}
+--shapes.triangle.right = {}
+shapes.regular = {}
+shapes.arrow = {}
+shapes.stars = {}
 
--- hexagon
-shapes.newHexagon = function( xx, yy, side )
+---- hexagon ----
+shapes.regular.hexagon = function( xx, yy, side )
     
     local vert1x = xx - ( side / 2 )
     local vert1y = yy + ( math.cos(60) * side )
@@ -24,8 +28,8 @@ shapes.newHexagon = function( xx, yy, side )
     return shape
 end
 
--- hocatgon
-shapes.newHoctagon = function( xx, yy, side )
+---- hocatgon ----
+shapes.regular.hoctagon = function( xx, yy, side )
     
     local vert1x = xx - ( side / 2 )
     local vert1y = yy - ( ( side / 2 ) + ( math.cos(45) * side ) )
@@ -51,8 +55,8 @@ shapes.newHoctagon = function( xx, yy, side )
     return shape
 end
 
--- equilateral triangle
-shapes.newTriangle.equi = function( xx, yy, side )
+---- equilateral triangle ----
+shapes.triangle.equi = function( xx, yy, side )
     
     local vert1x = xx - ( side / 2 )
     local vert1y = yy + ( math.sqrt(3) / 6 * side )
@@ -68,8 +72,8 @@ shapes.newTriangle.equi = function( xx, yy, side )
     return shape
 end
 
--- isosceles triangle
-shapes.newTriangle.isos = function( xx, yy, base, height )
+---- isosceles triangle ----
+shapes.triangle.isos = function( xx, yy, base, height )
     
     local vert1x = xx - ( base / 2 )
     local vert1y = yy + ( height / 3 )
@@ -85,8 +89,8 @@ shapes.newTriangle.isos = function( xx, yy, base, height )
     return shape
 end
 
--- right triangle
-shapes.newTriangle.right = function( xx, yy, base, height )
+---- right triangle ----
+shapes.triangle.right = function( xx, yy, base, height )
     
     local vert1x = xx - ( base / 3 )
     local vert1y = yy + ( height / 3 )
@@ -102,18 +106,18 @@ shapes.newTriangle.right = function( xx, yy, base, height )
     return shape
 end
 
--- pentagon
-shapes.newPentagon = function( xx, yy, side )
+---- pentagon ----
+shapes.regular.pentagon = function( xx, yy, side )
     
     local inR = side * 0.688
     local outR = side * 0.851
     
     local vert1x = xx - ( side / 2 )
-    local vert1y = yy + ( math.cos(36) * inR )
+    local vert1y = yy + inR
     local vert2x = xx + ( side / 2 )
-    local vert2y = yy + ( math.cos(36) * inR )
-    local vert3x = xx + ( math.cos(18) * outR )
-    local vert3y = yy - ( math.sin(18) * outR )
+    local vert2y = yy + inR
+    local vert3x = xx + ( math.cos(18) + outR )
+    local vert3y = yy - ( math.sin(18) + outR )
     local vert4x = xx  
     local vert4y = yy - outR
     local vert5x = xx - ( math.cos(18) + outR )
@@ -126,4 +130,100 @@ shapes.newPentagon = function( xx, yy, side )
     return shape
 end
 
+---- right arrow ----
+shapes.arrow.right = function( xx, yy, tWidth, tLenght, pWidth, pLenght )
+    
+    local vert1x = xx - tLenght
+    local vert1y = yy + ( tWidth / 2 )
+    local vert2x = xx
+    local vert2y = yy + ( tWidth / 2 )  
+    local vert3x = xx
+    local vert3y = yy + ( pWidth / 2 ) 
+    local vert4x = xx + pLenght
+    local vert4y = yy
+    local vert5x = xx
+    local vert5y = yy - ( pWidth / 2 )
+    local vert6x = xx
+    local vert6y = yy - ( tWidth / 2 )
+    local vert7x = xx - tLenght
+    local vert7y = yy - ( tWidth / 2 )
+    
+    local vertices = { vert1x, vert1y, vert2x, vert2y, vert3x, vert3y, vert4x, vert4y, vert5x, vert5y, vert6x, vert6y, vert7x, vert7y }
+    
+    local shape = display.newPolygon( xx, yy, vertices )
+    
+end
+
+---- left arrow ----
+shapes.arrow.left = function( xx, yy, tWidth, tLenght, pWidth, pLenght )
+    
+    local vert1x = xx + tLenght
+    local vert1y = yy - ( tWidth / 2 )
+    local vert2x = xx
+    local vert2y = yy - ( tWidth / 2 )  
+    local vert3x = xx
+    local vert3y = yy - ( pWidth / 2 ) 
+    local vert4x = xx - pLenght
+    local vert4y = yy
+    local vert5x = xx
+    local vert5y = yy + ( pWidth / 2 )
+    local vert6x = xx
+    local vert6y = yy + ( tWidth / 2 )
+    local vert7x = xx + tLenght
+    local vert7y = yy + ( tWidth / 2 )
+    
+    local vertices = { vert1x, vert1y, vert2x, vert2y, vert3x, vert3y, vert4x, vert4y, vert5x, vert5y, vert6x, vert6y, vert7x, vert7y }
+    
+    local shape = display.newPolygon( xx, yy, vertices )
+    
+end
+
+---- up arrow ----
+shapes.arrow.up = function( xx, yy, tWidth, tLenght, pWidth, pLenght )
+    
+    local vert1x = xx - ( tWidth / 2 )
+    local vert1y = yy + tLenght
+    local vert2x = xx - ( tWidth / 2 )
+    local vert2y = yy   
+    local vert3x = xx - ( pWidth / 2 )
+    local vert3y = yy  
+    local vert4x = xx 
+    local vert4y = yy - pLenght
+    local vert5x = xx + ( pWidth / 2 )
+    local vert5y = yy 
+    local vert6x = xx + ( tWidth / 2 )
+    local vert6y = yy 
+    local vert7x = xx + ( tWidth / 2 )
+    local vert7y = yy + tLenght
+    
+    local vertices = { vert1x, vert1y, vert2x, vert2y, vert3x, vert3y, vert4x, vert4y, vert5x, vert5y, vert6x, vert6y, vert7x, vert7y }
+    
+    local shape = display.newPolygon( xx, yy, vertices )
+    
+end
+
+---- down arrow ----
+shapes.arrow.down = function( xx, yy, tWidth, tLenght, pWidth, pLenght )
+    
+    local vert1x = xx + ( tWidth / 2 )
+    local vert1y = yy - tLenght
+    local vert2x = xx + ( tWidth / 2 )
+    local vert2y = yy   
+    local vert3x = xx + ( pWidth / 2 )
+    local vert3y = yy  
+    local vert4x = xx 
+    local vert4y = yy + pLenght
+    local vert5x = xx - ( pWidth / 2 )
+    local vert5y = yy 
+    local vert6x = xx - ( tWidth / 2 )
+    local vert6y = yy 
+    local vert7x = xx - ( tWidth / 2 )
+    local vert7y = yy - tLenght
+    
+    local vertices = { vert1x, vert1y, vert2x, vert2y, vert3x, vert3y, vert4x, vert4y, vert5x, vert5y, vert6x, vert6y, vert7x, vert7y }
+    
+    local shape = display.newPolygon( xx, yy, vertices )
+    
+end
+    
 return shapes
